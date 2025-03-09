455dee8a727e1c291162a7c37b1c7ee5e4d4df13
erick260392 <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>
2025-03-09 07:13:17 -0600

    create

--- Sites/wedding-invitations/routes/web.php
+++ Sites/wedding-invitations/routes/web.php
@@ -18,11 +18,11 @@ Route::middleware('auth')->group(function () {
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
     Route::post('/rsvp/{invitation}', [RsvpController::class, 'store'])->name('rsvp.store');
-    Route::get('/invitacion/{invitation:unique_code}', [InvitationController::class, 'show'])
-        ->name('invitation.show');
-          // O especifica manualmente:
     Route::get('/invitations/create', [InvitationController::class, 'create'])->name('invitations.create');
-    Route::post('/invitations', [InvitationController::class, 'store'])->name('invitations.store'); 
+    Route::post('/invitations', [InvitationController::class, 'store'])->name('invitations.store');
 });
+
+Route::get('/invitacion/{invitation:unique_code}', [InvitationController::class, 'show'])
+->name('invitation.show');
 
 require __DIR__ . '/auth.php';
